ce9b9704d7d54382757b68843fda1cb7f0a8bf7c
henok keraga <henokkeraga@gmail.com>
2024-04-11 11:45:22 -0600

    config server

---
+++ src/test/java/com/woinhen/configserver1/ConfigServer1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woinhen.configserver1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServer1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
